c3b125a73e091e4e84521e3ff063539d025330ff
Roberto Cardenas <dobetocoche@gmail.com>
2018-01-31 07:34:17 -0800

    .

---
+++ Lab1/Lab1_A.java
@@ -0,0 +1,16 @@
+/*
+Roberto Cardenas
+Math 130
+Lab1_A
+February 2, 2018
+*/
+public class Lab1_A
+{
+   public static void main(String [] args)
+   {
+      //This is a print line statement
+      System.out.println("Hello there, Roberto!");
+      //This is another print line statement
+      System.out.println("Math 130 rules!");
+   }
+}
---
+++ Lab1/Lab1_B.java
@@ -0,0 +1,22 @@
+/*
+Roberto Cardenas
+Math 130
+Lab1_B
+February 2, 2018
+*/
+public class Lab1_B
+{
+   public static void main(String []args)
+   {
+      //Start of massive block of print line statements
+   
+      System.out.println("My name is Roberto and I was born in Rosarito. I am 5' 6\"");
+      System.out.println("tall. My favorite son is: T.Q.M., by artist Little Jesus.");
+      System.out.println("I hope to graduate from San Diego State University majoring in");
+      System.out.println("Computer Science, and eventually go to work as a Programmer.");
+      System.out.println("Then, I plan on adopting an orca named Willy. After that, we");
+      System.out.println("both will live happily ever after!");
+      
+      //End of massive block of print line statements
+   }
+}
